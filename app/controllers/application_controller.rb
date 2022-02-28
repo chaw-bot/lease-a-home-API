a86626976694753e940f6da0583f1bda747b4ca0
wuyepabdul <wuyepabdul@gmail.com>
2022-02-28 14:51:14 +0100

    Setup Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class ApplicationController < ActionController::API
   before_action :update_allowed_parameters, if: :devise_controller?
 
--- app/models/jwt_denylist.rb
+++ app/models/jwt_denylist.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class JwtDenylist < ApplicationRecord
   include Devise::JWT::RevocationStrategies::Denylist
 
