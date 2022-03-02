897b94a26ba6870c9ca8709f4c2c66f304af1268
Whoistolu <digittolu25@gmail.com>
2022-03-02 01:27:03 +0100

    Re-organise code

--- spec/controller/user_controller_spec.rb
+++ spec/controller/user_controller_spec.rb
@@ -2,11 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User, type: :feature do
   describe 'User' do
-   
      
       subject { User.create!(name: "Henry", email: "henry@gmail.com", password: "henrykc") }
-    
-
       
       it { is_expected.to have_attributes(name: 'Henry') } 
   
