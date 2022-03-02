e5b45abdd4a92a05ce6ab3dce25c69408a9f400e
Whoistolu <digittolu25@gmail.com>
2022-03-02 01:35:29 +0100

    Fix liter errors

--- spec/controller/user_controller_spec.rb
+++ spec/controller/user_controller_spec.rb
@@ -2,19 +2,16 @@ require 'rails_helper'
 
 RSpec.describe User, type: :feature do
   describe 'User' do
-     
-      subject { User.create!(name: "Henry", email: "henry@gmail.com", password: "henrykc") }
-      
-    it { is_expected.to have_attributes(name: 'Henry') } 
-  
+    subject { User.create!(name: 'Henry', email: 'henry@gmail.com', password: 'henrykc') }
+
+    it { is_expected.to have_attributes(name: 'Henry') }
 
     it 'should validate user' do
       expect(subject).to be_valid
     end
-    
 
     it 'shows the user/s email' do
-      expect(subject).to have_attributes(email: 'henry@gmail.com') 
+      expect(subject).to have_attributes(email: 'henry@gmail.com')
     end
   end
 end
